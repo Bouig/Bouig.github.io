24090980b925b235adbae322e2bfbe6309902550
Camille <cnicolas@enssat.fr>
2022-02-08 15:44:56 +0100

    Début TP4 sur les workers

---
+++ TP5_JS4/Worker.js
@@ -0,0 +1,25 @@
+ /**Les variables*/
+ const xhr = new XMLHttpRequest();
+ var intervalID;
+ var max = 500;
+
+ intervalID = setInterval(getCoordinates, 1000);
+
+ /**Fonction */
+ function getCoordinates()
+ {
+     /**On récupère les coordonnées du cercle*/
+     let url = "https://stwom.herokuapp.com/"+max;
+     xhr.open("GET",url);
+     xhr.send();
+ }
+
+ /**Les événements*/
+ xhr.onreadystatechange = function()
+ {
+     if(this.readyState == this.DONE && this.status == 200)
+     {
+         var response = JSON.parse(this.responseText);
+         postMessage(response);
+     }
+ };
